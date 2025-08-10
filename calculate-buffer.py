5f56eb8c84d9583d886527d90068ff7bf3771c9d
justus237 <62303964+justus237@users.noreply.github.com>
2025-08-10 20:32:04 +0200

    portable removesuffix

--- calculate-buffer.py
+++ calculate-buffer.py
@@ -3,12 +3,19 @@
 #buffer (Bytes) = bandwidth_Mbit * RTT_ms * 1000 / 8
 
 import sys
+
+def remove_suffix(input_string, suffix):
+    if suffix and input_string.endswith(suffix):
+        return input_string[:-len(suffix)]
+    return input_string
+
+
 def calculate_buffer(bandwidth, delay1, delay2):
 	#rtt = delay1+delay2
 	if bandwidth.endswith("Mbit"):
-		bandwidth = int(bandwidth.removesuffix("Mbit"))
-		delay1 = int(delay1.removesuffix("ms"))
-		delay2 = int(delay2.removesuffix("ms"))
+		bandwidth = int(remove_suffix(bandwidth, "Mbit"))
+		delay1 = int(remove_suffix(delay1, "ms"))
+		delay2 = int(remove_suffix(delay2, "ms"))
 		rtt = delay1+delay2
 		buffer_bytes = int(rtt * bandwidth * 1000 / 8)
 		if buffer_bytes < 3000:
@@ -16,9 +23,9 @@ def calculate_buffer(bandwidth, delay1, delay2):
 		else:
 			print(buffer_bytes)
 	elif bandwidth.endswith("kbit"):
-		bandwidth = int(bandwidth.removesuffix("kbit"))
-		delay1 = int(delay1.removesuffix("ms"))
-		delay2 = int(delay2.removesuffix("ms"))
+		bandwidth = int(remove_suffix(bandwidth, "kbit"))
+		delay1 = int(remove_suffix(delay1, "ms"))
+		delay2 = int(remove_suffix(delay2, "ms"))
 		rtt = delay1+delay2
 		buffer_bytes = int(rtt * bandwidth / 8)
 		if buffer_bytes < 3000:
--- calculate-burst.py
+++ calculate-burst.py
@@ -4,16 +4,22 @@
 #burst (Bytes) = bandwidth_Mbit * 1000000/(250*8)
 
 import sys
+
+def remove_suffix(input_string, suffix):
+    if suffix and input_string.endswith(suffix):
+        return input_string[:-len(suffix)]
+    return input_string
+
 def calculate_burst(bandwidth):
 	if bandwidth.endswith("Mbit"):
-		bandwidth = bandwidth.removesuffix("Mbit")
+		bandwidth = remove_suffix(bandwidth, "Mbit")
 		burst = int(int(bandwidth) * 1000000/2000)
 		if burst < 3000:
 			print("3000")
 		else:
 			print(burst)
 	elif bandwidth.endswith("kbit"):
-		bandwidth = bandwidth.removesuffix("kbit")
+		bandwidth = remove_suffix(bandwidth, "kbit")
 		burst = int(int(bandwidth) * 1000/2000)
 		if burst < 3000:
 			print("3000")
