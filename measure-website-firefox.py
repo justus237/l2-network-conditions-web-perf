75a29bf1be3cb40cee35442541e0eed72a6b9d5a
justus <62303964+justus237@users.noreply.github.com>
2026-04-06 11:15:36 +0200

    reduce page load timeout

--- measure-website-firefox.py
+++ measure-website-firefox.py
@@ -220,7 +220,7 @@ def get_page_performance_metrics_and_write_logs(driver):
 def perform_page_load():
     driver = create_driver_with_default_options()
     # for now we set this really high because the defense implementation inflates PLTs by quite a bit...
-    driver.set_page_load_timeout(60)
+    driver.set_page_load_timeout(30)
     error = get_page_performance_metrics_and_write_logs(driver)
     log_file=log_dir+"firefox.moz_log"
     defense_state_dir = log_dir+"defense-state/"
